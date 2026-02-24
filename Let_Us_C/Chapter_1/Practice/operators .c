aad40be5f995811ab33750384987724fbcbeecdd
guvvala9h-arch <guvvala9h@gmail.com>
2026-02-24 20:48:58 +0530

    Add basic arithmetic operations in operators.c

---
+++ Let_Us_C/Chapter_1/Practice/operators .c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+  int a=3,b=5;
+printf("the sum of a and b is %d\n",a+b);
+printf("the difference of a and b is %d\n",a-b);
+printf("the product of a and b is%d\n",a*b);
+printf("the remainder when b is divided by a is\n"a%b);
+return0;
+}
