3e815de4dbd4b51379f59f5c5b0a0896d2ac999e
guvvala9h-arch <guvvala9h@gmail.com>
2026-03-06 15:08:54 +0530

    Add even/odd number check program
    Fix variable name typo and improve output message.

---
+++ Let_Us_C/chapter_2/practise/even_ odd_ number_ check .c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+  int num,flag=0;
+printf("enter a number \n");
+scanf("%d",&num);
+if(num%2==0)
+  flage=1;
+else flag=0;
+if(flag==1)
+  printf("the number you enter %d is even number\n");
+else printf("the number you entered %d is odd number\n");
+return 0;
+}
