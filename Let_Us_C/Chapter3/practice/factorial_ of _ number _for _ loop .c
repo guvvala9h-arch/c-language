74a9a9aa298348e3db4250e8907ceef3f3fbe172
guvvala9h-arch <guvvala9h@gmail.com>
2026-03-08 16:22:14 +0530

    Create factorial_ of _ number _for _ loop .c

---
+++ Let_Us_C/Chapter3/practice/factorial_ of _ number _for _ loop .c
@@ -0,0 +1,14 @@
+#inclide<stdio.h>
+int main()
+{
+  int num,fact=1;
+printf("enter a number\n");
+scanf("%d",&num);
+for(int i=1;i<=n;i++)
+{
+fact*=i;
+}
+printf("factorial of the number %d is %d",num,fact);
+
+return 0;
+}
