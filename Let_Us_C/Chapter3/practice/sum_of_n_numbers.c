b2ff01f32b7d2aa9c0bb1dd91d829a64e1ae04d5
guvvala9h-arch <guvvala9h@gmail.com>
2026-03-09 19:12:11 +0530

    Create sum_of_n_numbers.c

---
+++ Let_Us_C/Chapter3/practice/sum_of_n_numbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+  int n,sum=0;
+printf("enter a number\n");
+scanf("%d",&n);
+for(int i=1;i<=n;i++)
+  sum+=i;
+printf("sum of %d numbers is %d",n,sum);
+return 0;
+}
