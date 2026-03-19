3ef039709e71ae32ec4a3001bf1ab312298985f6
guvvala9h-arch <guvvala9h@gmail.com>
2026-03-19 12:03:26 +0530

    Create number_rectangle.c

---
+++ Let_Us_C/Chapter3/practice/number_rectangle.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+  int n,m;
+printf("enter number of rows and columns");
+scanf("%d %d",&n,&m);
+for(int i=1;i<=n;i++)
+{for(int j=1;j<=m;j++)
+printf("%d",j);
+printf("\n");
+}
+return 0;
+}
