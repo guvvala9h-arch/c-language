368a25b0cff4eaaa7a113fb244b172a987cac584
guvvala9h-arch <guvvala9h@gmail.com>
2026-04-21 06:44:26 +0530

    Create bit_wise_ operator.c

---
+++ Let_Us_C/chapter_2/practise/bit_wise_ operator.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+main()
+{//bitwise operators 
+int a,b,c,d,e,f;
+printf("enter a\n");
+scanf("%d",&a);
+printf("enter b\n");
+scanf("%d",&b);
+printf("a&b=%d\n",a&b);
+printf("a|b=%d\n",a|b);
+printf("a^b=%d\n",a^b);
+printf("a<<5=%d\n",a<<5);
+printf("a>>4=%d\n",a>>4);
+printf("b<<5=%d\n",b<<5);
+printf("b>>3=%d",b>>3);
+return 0;
+}
