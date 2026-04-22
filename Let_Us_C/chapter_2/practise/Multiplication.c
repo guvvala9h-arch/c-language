90350b8c3574e0faf6ef3fe2c6f8990cc736f3f6
guvvala9h-arch <guvvala9h@gmail.com>
2026-04-22 20:34:24 +0530

    Create Multiplication.c

---
+++ Let_Us_C/chapter_2/practise/Multiplication.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+int A,B,C,D,E,F,G,i,J;
+int result_a,result_b,result_c,result_d;
+printf("enter values of A,B,C,D,E,F,G:\n");
+scanf("%d%d%d%d%d%d%d",&A,&B,&C,&D,&E,&F,&G);
+printf("enter value for i:\n");
+scanf("%d",&i);
+result_a=A+B*C+(D*E)+F*G;
+result
