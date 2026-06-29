eb47402bbb1538fdde98f91cafd88f519325e124
Guvvala Harshitha <guvvala9h@gmail.com>
2026-06-29 20:41:40 +0530

    Add Pulse counter program in C

---
+++ Let_Us_C/Chapter6/Pulse counter.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int pulses;
+
+    printf("Enter Number of Pulses: ");
+    scanf("%d", &pulses);
+
+    printf("Total Pulses Counted = %d\n", pulses);
+
+    return 0;
+}
