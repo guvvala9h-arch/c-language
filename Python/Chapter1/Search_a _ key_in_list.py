32a87ab41a041b7e152d5e429a03cd9549d9096d
Guvvala Harshitha <guvvala9h@gmail.com>
2026-08-21 21:43:50 +0530

    Add key search functionality in student dictionary

---
+++ Python/Chapter1/Search_a _ key_in_list.py
@@ -0,0 +1,7 @@
+student={"name":"vyshnavi","age":20,"branch":"cse"}
+key=input("enter key to search:")
+if key in student:
+    print("key exists")
+else :
+    print("key doesnot exists")   
+     
